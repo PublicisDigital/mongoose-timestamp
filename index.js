5aa652b42482d48912a3e81fcd212c26b7f52394
Andrew Apperley <andrew.apperley@publicis.ca>
2015-06-12 17:15:15 -0400

    Added publish check

--- index.js
+++ index.js
@@ -29,70 +29,72 @@ function historyPlugin(schema, addHistory) {
 
         var updateHappened = function(object, next) {
 
-            HistoryModel.find({objectId: object._id}, {}, {sort: {updatedAt: -1}})
-                .limit(1)
-                .exec(function(err, history) {
-                if (err) {
-                    console.log("Err:: ", err);
-                }
-                history = history[0];
-
-                var blacklistedKeys = ["_id", "updatedAt", "updatedBy", "createdAt", "owner", "__v", "meta"];
-                function onBlacklist(key) {
-                    var blacklistedKey = false;
-                    _.forEach(blacklistedKeys, function(k) {
-                        if (k == key) {
-                            blacklistedKey = true;
-                        }
-                    });
-                    return blacklistedKey;
-                }
-
-                    var different = false;
-                    if (history !== undefined) {
-
-                        _.forIn(history.object, function(value, key) {
-
-                            if (!onBlacklist(key)) {
-                                console.log("CURRENT OBJECT:: KEY:: ", key, "VALUE:: ", object[key]);
-                                console.log("NEW OBJECT:: KEY:: ", key, "VALUE:: ", value);
-                                if (!_.isEqual(value, object[key]))  {
-                                    console.log("FOUND DIFFERENCE FOR OBJECT/ARRAY");
-                                    different = true;
-                                }
-                            }
-                        });
-                    } else {
-                        different = true;
-                    }
-
-
-                if (different) {
-                    var action;
-
-                    if (object.deleted === true) {
-                        action = "Delete";
-                    } else {
-                        action = "Update";
-                    }
-                    var newHistory = new HistoryModel({
-                        action: action,
-                        object: object,
-                        owner: object.updatedBy
-                    });
-                    newHistory.save(function(err, __history) {
+            if (object.published) {
+                HistoryModel.find({objectId: object._id}, {}, {sort: {updatedAt: -1}})
+                    .limit(1)
+                    .exec(function(err, history) {
                         if (err) {
-                            console.log(err);
+                            console.log("Err:: ", err);
                         }
-                        next();
+                        history = history[0];
+
+                        var blacklistedKeys = ["_id", "updatedAt", "updatedBy", "createdAt", "owner", "__v", "meta", "published"];
+                        function onBlacklist(key) {
+                            var blacklistedKey = false;
+                            _.forEach(blacklistedKeys, function(k) {
+                                if (k == key) {
+                                    blacklistedKey = true;
+                                }
+                            });
+                            return blacklistedKey;
+                        }
+
+                        var different = false;
+                        if (history !== undefined) {
+
+                            _.forIn(history.object, function(value, key) {
+
+                                if (!onBlacklist(key)) {
+                                    console.log("CURRENT OBJECT:: KEY:: ", key, "VALUE:: ", object[key]);
+                                    console.log("NEW OBJECT:: KEY:: ", key, "VALUE:: ", value);
+                                    if (!_.isEqual(value, object[key]))  {
+                                        console.log("FOUND DIFFERENCE FOR OBJECT/ARRAY");
+                                        different = true;
+                                    }
+                                }
+                            });
+                        } else {
+                            different = true;
+                        }
+
+
+                        if (different) {
+                            var action;
+
+                            if (object.deleted === true) {
+                                action = "Delete";
+                            } else {
+                                action = "Update";
+                            }
+                            var newHistory = new HistoryModel({
+                                action: action,
+                                object: object,
+                                owner: object.updatedBy
+                            });
+                            newHistory.save(function(err, __history) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                next();
+                            });
+                        } else {
+                            next();
+                        }
+
                     });
-                } else {
-                    next();
-                }
-
-
-
-            });
+            } else {
+                next();
+            }
         };
 
         var saveHappened = function(object, next) {
